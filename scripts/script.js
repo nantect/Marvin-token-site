4ddc3ab469afbc7024a5bc94cbbeb99e78d21f21
nantect <nantectx@gmail.com>
2024-09-29 15:30:01 +0100

    initial site commit

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// Smooth scroll for navigation
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
